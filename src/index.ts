69cce36216b7bf3855d4266365264d0cf07dfb00
leonardperez1992 <leonardperez1992@gmail.com>
2023-12-04 18:23:07 -0500

    Cambios 4-12

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-import { getPokemon } from "./generics/getPokemon";
+// import { getPokemon } from "./generics/getPokemon";
 
-getPokemon(1).then(pokemon => console.log(pokemon.sprites.front_default))
-            .catch(error => console.log(error)) 
-            .finally(()=> console.log('Fin de GetPokemon'))
+// getPokemon(1).then(pokemon => console.log(pokemon.sprites.front_default))
+//             .catch(error => console.log(error)) 
+//             .finally(()=> console.log('Fin de GetPokemon'))
+
+import { Pokemon } from "./decorators/pokemon-clas";
+
+const charmander = new Pokemon('Charmander');
+
+console.log(charmander);
+
